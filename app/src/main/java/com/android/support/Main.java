3d2cf336a4748abb8be5cf2999d9a62d2c9720d0
Alexjr2 <52109110+Alexjr2@users.noreply.github.com>
2024-07-19 15:25:04 +0800

    Update Main.java

--- app/src/main/java/com/android/support/Main.java
+++ app/src/main/java/com/android/support/Main.java
@@ -1,21 +1,65 @@
 package com.android.support;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
+import android.content.res.AssetManager;
+import android.util.Log;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Handler;
 import android.provider.Settings;
 import android.widget.Toast;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Arrays;
 
+@SuppressWarnings("all")
 public class Main {
+    public static final String MODMENU = "libmodmenu";
 
-    //Load lib
-    static {
-        // When you change the lib name, change also on Android.mk file
-        // Both must have same name
-        System.loadLibrary("MyLibName");
+    public static String libmodmenu() {
+        String arch = System.getProperty("os.arch");
+        if (arch != null && (arch.contains("aarch64") || arch.contains("x86_64"))) {
+            return "libmodmenu64.so";
+        } else {
+            return "libmodmenu32.so";
+        }
+    }
+
+    public static void loadLibFromAssets(Context context) throws RuntimeException, IOException {
+
+        if (context == null) {
+            return;
+        }
+
+        String dummyLib = libmodmenu();
+
+        AssetManager assetManager = context.getAssets();
+        String[] list = assetManager.list(MODMENU);
+        
+        if (list == null) {
+            throw new RuntimeException("Asset list is null");
+        }
+
+        String listToString = Arrays.toString(list);
+
+        if (!listToString.contains(dummyLib)) {
+            throw new RuntimeException("Unable to locate file");
+        } else {
+            try (InputStream is = assetManager.open(MODMENU.concat("/").concat(dummyLib));
+                 OutputStream os = new FileOutputStream(new File(context.getDataDir().getAbsolutePath(), dummyLib))) {
+                transfer(is, os);
+                doLoad(new File(context.getDataDir().getAbsolutePath(), dummyLib));
+            } catch (Exception exception) {
+                Log.e("EXR", "loadLibFromAssets: ", exception);
+                throw new RuntimeException("Crappy crap happened");
+            }
+        }
     }
 
     private static native void CheckOverlayPermission(Context context);
@@ -24,7 +68,7 @@ public class Main {
         CrashHandler.init(context, true);
         if (context instanceof Activity) {
             //Check if context is an Activity.
-            Menu menu = new Menu(context);
+            menu = new Menu(context);
             menu.SetWindowManagerActivity();
             menu.ShowMenu();
         } else {
@@ -35,7 +79,27 @@ public class Main {
 
     public static void Start(Context context) {
         CrashHandler.init(context, false);
-
         CheckOverlayPermission(context);
+        try {
+            loadLibFromAssets(context);
+        } catch (Throwable e) {
+            throw new ExceptionInInitializerError(e);
+        }
+    }
+    @SuppressLint("UnsafeDynamicallyLoadedCode")
+    private static void doLoad(File outFile) {
+        try {
+            System.load(outFile.getAbsolutePath());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void transfer(InputStream is, OutputStream os) throws IOException {
+        byte[] buffer = new byte[8192];
+        int n;
+        while ((n = is.read(buffer)) != -1) {
+            os.write(buffer, 0, n);
+        }
     }
 }
