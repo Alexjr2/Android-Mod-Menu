a5b8b37ff349f27049bc0de72d796433d96facaf
Alexjr2 <52109110+Alexjr2@users.noreply.github.com>
2024-07-19 15:14:59 +0800

    Update MainActivity.java

--- app/src/main/java/com/android/support/MainActivity.java
+++ app/src/main/java/com/android/support/MainActivity.java
@@ -36,7 +36,7 @@ public class MainActivity extends Activity {
             } catch (ClassNotFoundException e) {
                 Log.e("Mod_menu", "Error. Game's main activity does not exist");
                 //Uncomment this if you are following METHOD 2 to launch menu
-                //Toast.makeText(MainActivity.this, "Error. Game's main activity does not exist", Toast.LENGTH_LONG).show();
+                Toast.makeText(MainActivity.this, "Error. Game's main activity does not exist", Toast.LENGTH_LONG).show();
             }
         }
 
