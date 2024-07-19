90b79e6ef30438e52005173650319d422800210f
Alexjr2 <52109110+Alexjr2@users.noreply.github.com>
2024-07-19 15:35:44 +0800

    Update Setup.h

--- app/src/main/jni/Menu/Setup.h
+++ app/src/main/jni/Menu/Setup.h
@@ -109,12 +109,12 @@ void CheckOverlayPermission(JNIEnv *env, jclass thiz, jobject ctx){
 
 void Init(JNIEnv *env, jobject thiz, jobject ctx, jobject title, jobject subtitle){
     //Set sub title
-    setText(env, title, OBFUSCATE("<b>Modded by (yourname)</b>"));
+    setText(env, title, OBFUSCATE("<b>Android LGL ModMenu</b>"));
 
     //Set sub title
     setText(env, subtitle, OBFUSCATE("<b><marquee><p style=\"font-size:30\">"
-                                     "<p style=\"color:green;\">Modded by LGL</p> | "
-                                     "https://github.com/LGLTeam | Lorem Ipsum is simply dummy text of the printing and typesetting</p>"
+                                     "<p style=\"color:green;\">Android LGL ModMenu</p> | "
+                                     "https://t.me/Rindaman5555 | Telegram</p>"
                                      "</marquee></b>"));
 
     //Dialog Example
